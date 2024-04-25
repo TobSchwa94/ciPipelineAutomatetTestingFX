5f3bfc1dcaf02cec4acd52466a2e82d8b5e4d712
blackPanther <herrt@dhcp-wlce193128.zim.uni-duisburg-essen.de>
2024-04-25 10:38:13 +0100

    first code

---
+++ src/app/Calculator.java
@@ -0,0 +1,33 @@
+package app;
+
+public class Calculator {
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        // Test der Methoden
+        System.out.println("Addition: " + calculator.add(5, 3));
+        System.out.println("Subtraction: " + calculator.subtract(5, 3));
+        System.out.println("Multiplication: " + calculator.multiply(5, 3));
+        System.out.println("Division: " + calculator.divide(10, 2));
+    }
+}
